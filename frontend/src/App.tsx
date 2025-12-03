87b252c2b17572589e8145ff49a4f5bf3b6e9f32
0gene68 <yeonghun8309@gmail.com>
2025-12-04 02:11:17 +0900

    Delete unnecessary code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,15 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
-import type { User } from "./../../backend/server";
 import klLogo from "./assets/img/klLogo.svg";
 import fzLogo from "./assets/img/fzLogo.svg";
 import ctLogo from "./assets/img/ctLogo.svg";
 import ssLogo from "./assets/img/ssLogo.svg";
 import csLogo from "./assets/img/csLogo.svg";
+import type { Member } from "./../../backend/src/types/Member";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<Member[]>([]);
 
   // 백엔드에서 mock 데이터 가져오기
   useEffect(() => {
@@ -33,24 +30,6 @@ function App() {
         <img src={ssLogo} className="logo" alt="Sweet Steady logo" />
         <img src={csLogo} className="logo" alt="Cutie Street logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <h1>Users</h1>
-      <ul>
-        {users.map((u) => (
-          <li key={u.id}>{u.name}</li>
-        ))}
-      </ul>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
