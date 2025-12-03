a85f00dc0b29d7ed306ab08749512282d85621ef
0gene68 <yeonghun8309@gmail.com>
2025-12-04 04:11:08 +0900

    hover 시 그룹명 수정

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,31 +1,20 @@
 import klLogo from "../assets/img/klLogo.svg";
 import fzLogo from "../assets/img/fzLogo.svg";
-// import ctLogo from "../assets/img/ctLogo.svg";
-// import ssLogo from "../assets/img/ssLogo.svg";
-// import csLogo from "../assets/img/csLogo.svg";
+import ctLogo from "../assets/img/ctLogo.svg";
+import ssLogo from "../assets/img/ssLogo.svg";
+import csLogo from "../assets/img/csLogo.svg";
 import LogoCard from "../components/LogoCard";
-import { useEffect, useState } from "react";
-import type { Team } from "./../../../backend/src/types/Team";
-import axios from "axios";
 import "../css/Home.css";
 
 function Home() {
-  const [teams, setTeams] = useState<Team[]>([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:4000/api/teams")
-      .then((res) => setTeams(res.data))
-      .catch((err) => console.error(err));
-  }, []);
-
   return (
     <>
       <img src={klLogo} id="klLogo" alt="Kawaii Lab Logo" />
       <div>
-        {teams.map((team) => (
-          <LogoCard img={fzLogo} text={team.name} />
-        ))}
+        <LogoCard img={fzLogo} text={"FRUITS ZIPPER"} />
+        <LogoCard img={ctLogo} text={"CANDY TUNE"} />
+        <LogoCard img={ssLogo} text={"SWEET STEADY"} />
+        <LogoCard img={csLogo} text={"CUTIE STREET"} />
       </div>
     </>
   );
