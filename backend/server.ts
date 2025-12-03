dca5c4361b7a302650068722e30dfea274584b0d
0gene68 <yeonghun8309@gmail.com>
2025-12-04 02:10:20 +0900

    Separate connection code and data code

--- backend/server.ts
+++ backend/server.ts
@@ -1,30 +1,20 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
+import { fzMembers } from "./data/fzMembers";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// TS 타입 지정
-export interface User {
-  id: number;
-  name: string;
-}
-
-let users: User[] = [
-  { id: 1, name: "Alice" },
-  { id: 2, name: "Bob" },
-];
-
 app.get("/api/users", (req: Request, res: Response) => {
-  res.json(users);
+  res.json(fzMembers);
 });
 
-app.post("/api/users", (req: Request, res: Response) => {
-  const newUser: User = { id: users.length + 1, ...req.body };
-  users.push(newUser);
-  res.json(newUser);
-});
+// app.post("/api/users", (req: Request, res: Response) => {
+//   const newUser: Member = { id: users.length + 1, ...req.body };
+//   users.push(newUser);
+//   res.json(newUser);
+// });
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
