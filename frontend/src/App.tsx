627b2ae45adc77dbe41f9d3803bd8d27d834d6dc
0gene68 <yeonghun8309@gmail.com>
2025-12-04 03:21:30 +0900

    Added display of team name when hovering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,31 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-import axios from "axios";
 import klLogo from "./assets/img/klLogo.svg";
 import fzLogo from "./assets/img/fzLogo.svg";
 import ctLogo from "./assets/img/ctLogo.svg";
 import ssLogo from "./assets/img/ssLogo.svg";
 import csLogo from "./assets/img/csLogo.svg";
-import type { Member } from "./../../backend/src/types/Member";
 
 function App() {
-  const [users, setUsers] = useState<Member[]>([]);
-
-  // 백엔드에서 mock 데이터 가져오기
-  useEffect(() => {
-    axios
-      .get("http://localhost:4000/api/users")
-      .then((res) => setUsers(res.data))
-      .catch((err) => console.error(err));
-  }, []);
-
   return (
     <>
       <img src={klLogo} id="klLogo" alt="Kawaii Lab Logo" />
       <div>
-        <img src={fzLogo} className="logo" alt="Fruits Zipper logo" />
-        <img src={ctLogo} className="logo" alt="Candy Tune logo" />
-      </div>
-      <div>
-        <img src={ssLogo} className="logo" alt="Sweet Steady logo" />
-        <img src={csLogo} className="logo" alt="Cutie Street logo" />
+        <div className="logo-container">
+          <img src={fzLogo} className="logo" alt="Fruits Zipper logo" />
+          <span className="tooltip-text">Fruits Zipper</span>
+        </div>
+        <div className="logo-container">
+          <img src={ctLogo} className="logo" alt="Candy Tune logo" />
+          <span className="tooltip-text">Candy Tune</span>
+        </div>
+        <div className="logo-container">
+          <img src={ssLogo} className="logo" alt="Sweet Steady logo" />
+          <span className="tooltip-text">Sweet Steady</span>
+        </div>
+        <div className="logo-container">
+          <img src={csLogo} className="logo" alt="Cutie Street logo" />
+          <span className="tooltip-text">Cutie Street</span>
+        </div>
       </div>
     </>
   );
