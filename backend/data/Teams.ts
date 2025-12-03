9d05824f9f978de9f97e6c9c2ec922adbe0210ed
0gene68 <yeonghun8309@gmail.com>
2025-12-04 03:20:39 +0900

    Add Team data

---
+++ backend/data/Teams.ts
@@ -0,0 +1,28 @@
+import type { Team } from "./../src/types/Team";
+
+export let teams: Team[] = [
+  {
+    id: 1,
+    name: "FRUITS ZIPPER",
+    memberNum: 7,
+    debutDate: new Date(2022, 4, 10),
+  },
+  {
+    id: 2,
+    name: "CANDY TUNE",
+    memberNum: 7,
+    debutDate: new Date(2023, 3, 14),
+  },
+  {
+    id: 3,
+    name: "SWEET STEADY",
+    memberNum: 7,
+    debutDate: new Date(2022, 4, 10),
+  },
+  {
+    id: 4,
+    name: "CUTIE STREET",
+    memberNum: 8,
+    debutDate: new Date(2022, 4, 10),
+  },
+];
