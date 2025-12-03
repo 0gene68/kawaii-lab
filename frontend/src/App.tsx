7677ca17d3a325fa9a77bf901c65803a2d59ec15
0gene68 <yeonghun8309@gmail.com>
2025-12-04 03:54:55 +0900

    App.tsx와 홈 화면 분리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,12 @@
-import "./App.css";
-import klLogo from "./assets/img/klLogo.svg";
-import fzLogo from "./assets/img/fzLogo.svg";
-import ctLogo from "./assets/img/ctLogo.svg";
-import ssLogo from "./assets/img/ssLogo.svg";
-import csLogo from "./assets/img/csLogo.svg";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <img src={klLogo} id="klLogo" alt="Kawaii Lab Logo" />
-      <div>
-        <div className="logo-container">
-          <img src={fzLogo} className="logo" alt="Fruits Zipper logo" />
-          <span className="tooltip-text">Fruits Zipper</span>
-        </div>
-        <div className="logo-container">
-          <img src={ctLogo} className="logo" alt="Candy Tune logo" />
-          <span className="tooltip-text">Candy Tune</span>
-        </div>
-        <div className="logo-container">
-          <img src={ssLogo} className="logo" alt="Sweet Steady logo" />
-          <span className="tooltip-text">Sweet Steady</span>
-        </div>
-        <div className="logo-container">
-          <img src={csLogo} className="logo" alt="Cutie Street logo" />
-          <span className="tooltip-text">Cutie Street</span>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
