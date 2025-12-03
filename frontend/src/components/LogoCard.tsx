3013d8181ddf5a5327635814006f9a001d476e31
0gene68 <yeonghun8309@gmail.com>
2025-12-04 04:36:54 +0900

    컴포넌트에 팀별 페이지 이동 기능 추가

--- frontend/src/components/LogoCard.tsx
+++ frontend/src/components/LogoCard.tsx
@@ -1,13 +1,16 @@
 interface LogoCardProps {
   img: string;
+  url: string;
   text: string;
 }
 
-export default function LogoCard({ img, text }: LogoCardProps) {
+export default function LogoCard({ img, url, text }: LogoCardProps) {
   return (
-    <div className="logo-container">
-      <img src={img} className="logo" />
-      <span className="tooltip-text">{text}</span>
-    </div>
+    <a href={url}>
+      <div className="logo-container">
+        <img src={img} className="logo" />
+        <span className="tooltip-text">{text}</span>
+      </div>
+    </a>
   );
 }
