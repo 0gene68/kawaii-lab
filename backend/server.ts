881ba8ac227570d1aaa7fa509d950652f57d85e4
0gene68 <yeonghun8309@gmail.com>
2025-12-04 06:02:55 +0900

    FRUITS ZIPPER api 추가

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,14 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
 import teamsRouter from "./api/teams";
+import fzRouter from "./api/fz";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/teams", teamsRouter);
+app.use("/api/FruitsZipper", fzRouter);
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
