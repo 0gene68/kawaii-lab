97f639f108d324124f23b426c7fbd42db4b2ef76
0gene68 <yeonghun8309@gmail.com>
2025-12-04 03:20:21 +0900

    Team information API code separation

--- backend/server.ts
+++ backend/server.ts
@@ -1,20 +1,12 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
-import { fzMembers } from "./data/fzMembers";
+import teamsRouter from "./api/teams";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/users", (req: Request, res: Response) => {
-  res.json(fzMembers);
-});
-
-// app.post("/api/users", (req: Request, res: Response) => {
-//   const newUser: Member = { id: users.length + 1, ...req.body };
-//   users.push(newUser);
-//   res.json(newUser);
-// });
+app.use("/api/teams", teamsRouter);
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
