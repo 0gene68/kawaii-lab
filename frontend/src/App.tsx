a6ba8a4ffad780b2903cf24186bc2c2236dbe5dd
0gene68 <yeonghun8309@gmail.com>
2025-12-04 04:35:18 +0900

    팀별 페이지 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import FruitsZipper from "./pages/FruitsZipper";
+import CandyTune from "./pages/CandyTune";
+import SweetSteady from "./pages/SweetSteady";
+import CutieStreet from "./pages/CutieStreet";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/FruitsZipper" element={<FruitsZipper />} />
+        <Route path="/CandyTune" element={<CandyTune />} />
+        <Route path="/SweetSteady" element={<SweetSteady />} />
+        <Route path="/CutieStreet" element={<CutieStreet />} />
       </Routes>
     </>
   );
